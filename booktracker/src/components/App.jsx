dd315b0ad9bf857d842f26b837c74d6a45d70461
Ariel <ariel.oliva3748@gmail.com>
2025-09-18 20:49:07 -0300

    Inicio proyecto

---
+++ booktracker/src/components/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from '/src/assets/react.svg'
+import viteLogo from '/vite.svg'
+import '../styles/App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
